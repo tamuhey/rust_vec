8adaf86f02facb9413df6bf97cf39ea205dfa1d6
Yohei Tamura <tamuhey@gmail.com>
2020-08-30 23:26:04 +0900

    add grow

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#![feature(ptr_internals)]
+#![feature(alloc_internals)]
+use std::alloc::{self, Layout};
+use std::marker::PhantomData;
+use std::mem;
+use std::ptr::{self, Unique};
+
+struct Vec<T> {
+    ptr: Unique<T>,
+    cap: usize,
+    len: usize,
+}
+
+impl<T> Vec<T> {
+    fn new() -> Self {
+        if mem::size_of::<T>() == 0 {
+            unimplemented!("ZST is unsupported")
+        }
+        Self {
+            ptr: Unique::dangling(),
+            len: 0,
+            cap: 0,
+        }
+    }
+    fn grow(&mut self) {
+        unsafe {
+            let layout = Layout::new::<T>();
+            let (new_cap, new_ptr) = if self.cap == 0 {
+                (1, alloc::alloc(layout))
+            } else {
+                let new_cap = self.cap * 2;
+
+                if self.cap * layout.size() >= std::isize::MAX as usize {
+                    // Since LLVM doesn't have unsigned integer type, the allowed maximum usize is isize:MAX
+                    panic!("capacity overflow");
+                }
+                let new_ptr = alloc::realloc(self.ptr.as_ptr() as *mut _, layout, new_cap);
+                if new_ptr.is_null() {
+                    alloc::rust_oom(layout);
+                }
+                (new_cap, new_ptr)
+            };
+            self.ptr = Unique::new(new_ptr as *mut T).unwrap();
+            self.cap = new_cap;
+        }
+    }
+}
+
+fn main() {
+    let mut a = Vec::<usize>::new();
+    a.grow();
+    assert!(a.cap == 1);
+    a.grow();
+    assert!(a.cap == 2);
+    println!("OK!");
+}
